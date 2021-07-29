9582cde8f6a83795e05788aaab7f1d8bd3278a6d
CptPhenomeno <benvenutigab@gmail.com>
2021-07-29 07:09:52 +0200

    feat: added searchpanel solution

---
+++ src/SearchPanel/TestSearchPanel.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
+using Xamarin.Forms.Xaml;
+
+namespace SearchPanel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestSearchPanel : PancakeView
+    {
+        public TestSearchPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
